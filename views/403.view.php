a24cadaca51f3b4a724a3e7cf07a395ed4283121
Andreja Trajkovic <andreja@192.168.1.28>
2024-12-26 20:01:09 +0100

    Initial commit

---
+++ views/403.view.php
@@ -0,0 +1,13 @@
+<?php require("views/partials/head.php"); ?>
+<?php require("views/partials/nav.php"); ?>
+
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h2 class="text-2xl font-bold">You are not authorized to view this page.</h2>
+        <p class="mt-4">
+            <a href="/" class="text-blue-500 underline">Go back to Home page.</a>
+        </p>
+    </div>
+</main>
+<?php require("views/partials/foot.php"); ?>
---
+++ views/404.view.php
@@ -0,0 +1,14 @@
+<?php require base_path("views/partials/head.php"); ?>
+<?php require base_path("views/partials/nav.php"); ?>
+
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl my-10">404 Page not found</h1>
+        <h2 class="text-2xl font-bold">Sorry. Page Not Found.</h2>
+        <p class="mt-4">
+            <a href="/" class="text-blue-500 underline">Go back to Home page.</a>
+        </p>
+    </div>
+</main>
+<?php require base_path("views/partials/foot.php"); ?>
