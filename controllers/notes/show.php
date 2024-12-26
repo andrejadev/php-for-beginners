3c82d2a3a618769821fb7831355bb773022341c0
andreja <andrejadev@gmail.com>
2024-12-26 21:23:15 +0100

    Implementing the feature to delete a note.

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,18 +5,30 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-
 $currentUserId = 3;
 
-$note = $db->query('SELECT * from notes where id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $note = $db->query('SELECT * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+    authorize($note['user_id'] === $currentUserId);
+    $db->query('DELETE FROM notes WHERE id = :id', [
+        ':id' => $_GET['id']
+    ]);
+    header('Location: /notes');
+    exit();
+} else {
 
-view("notes/show.view.php",[
-    'heading' => 'Note',
-    'note' => $note,
-]);
+    $note = $db->query('SELECT * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
 
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note,
+    ]);
+}
 
