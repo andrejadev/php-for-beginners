f31be883e0e6e7c1f77360c65932b0ee791bbda9
andreja <andrejadev@gmail.com>
2025-01-09 22:24:52 +0100

    New Feature - Register a New User (Part 2) - Creating a store.php controller    - Validate the form inputs    - Check if the account already exists       - if yes, redirect to home page       - if not, save one to the database, and then log the user in, and redirect    - mark that the user has logged in(using sessions)
    - Modifying `nav.php` partial code
        - Shows the avatar + text: “Welcome, {username}” if the user is logged in
        - if not, show the register button

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -1,26 +1,49 @@
 <?php
 
+use Core\Database;
 use Core\Validator;
+use Core\App;
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 //validate the form inputs
 $errors = [];
-if(!Validator::email($email)){
+if (!Validator::email($email)) {
     $errors['email'] = 'Please enter a valid email address';
 }
 
-if(!Validator::string($password,7, 255)){
+if (!Validator::string($password, 7, 255)) {
     $errors['password'] = 'Please provide a password of at least 7 characters';
 }
 
-if(!empty($errors)){
+if (!empty($errors)) {
     return view('registration/create.view.php', [
         'errors' => $errors
     ]);
 }
 
+$db = App::resolve(Database::class);
 //Check if the account already exists
-    //if yes, redirect to login page
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+if ($user) {
+    //then someone with that email already exists and has an account.
+    //if yes, redirect to home page
+    header("location: /");
+    exit();
+} else {
     //if not, save one to the database, and then log the user in, and redirect
+    $db -> query('insert into users (email, password) values (:email, :password)', [
+        'email' => $email,
+        'password' => $password
+    ]);
+
+    //mark that the user has logged in
+    $_SESSION['user'] = [
+        'email' => $email,
+    ];
+    header("location: /");
+    exit();
+}
