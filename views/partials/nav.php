3319e81c50138954c1720805407fc705d7605ca8
andreja <andrejadev@gmail.com>
2025-02-11 19:29:34 +0100

    Design fixes - Excluding user menu dropdown - Fixing the tailwind logo

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,7 +3,7 @@
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
                 <div class="shrink-0">
-                    <img class="size-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
+                    <img class="size-8" src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.d52e9897.svg"
                          alt="Your Company">
                 </div>
                 <div class="hidden md:block">
@@ -62,17 +62,6 @@
                             From: "transform opacity-100 scale-100"
                             To: "transform opacity-0 scale-95"
                         -->
-                        <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none"
-                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
-                             tabindex="-1">
-                            <!-- Active: "bg-gray-100 outline-none", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                               id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                               id="user-menu-item-1">Settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                               id="user-menu-item-2">Sign out</a>
-                        </div>
                     </div>
                 </div>
             </div>
