54858a6b91c6c72a3df140960febba0652516ec8
andreja <andrejadev@gmail.com>
2025-02-13 19:42:44 +0100

    Quick fix for the commit: d46843de8a7093119215e7835ed4b4ee16c3ebb7 - Excluding commented code

--- Http/controllers/sessions/store.php
+++ Http/controllers/sessions/store.php
@@ -18,19 +18,6 @@ if (!$form->validate($email, $password)) {
     ]);
 }
 
-/*$errors = [];
-if (!Validator::email($email)) {
-    $errors['email'] = 'Please enter a valid email address';
-}
-
-if (!Validator::string($password)) {
-    $errors['password'] = 'Please provide a valid password';
-}
-
-if (!empty($errors)) {
-    return view('sessions/create.view,php', ['errors' => $errors]);
-}*/
-
 //check the credentials
 $user = $db->query('SELECT * FROM users WHERE email = :email', [
     'email' => $email
