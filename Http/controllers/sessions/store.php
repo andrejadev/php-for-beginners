ed88a9aba7cae0fa57916ec0a804cf40d1f6974d
andreja <andrejadev@gmail.com>
2025-02-16 13:47:11 +0100

    Extracting an Authenticator Class
    - Creating a new `Authenticator class`
        - Taking a the login logic from sessions/store.php controller and migrating it to the Authenticator class
        - Taking `login` and `logout` fns from functions.php and migrating it to the Authenticator class
    - Creating a logic where Authenticator class will handle user authentication and store controller will handle the redirection
        - Creating a helper fn in our functions.php file for redirection to the home page.
    - Refactoring the code for displaying errors when redirected to the login page
        - We are adding new method in `LoginForm class`

--- Http/controllers/sessions/store.php
+++ Http/controllers/sessions/store.php
@@ -1,42 +1,25 @@
 <?php
 
 //log in the user if the credentials match
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
-
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
-if (!$form->validate($email, $password)) {
-    return view('sessions/create.view,php', [
-        'errors' => $form->errors()
-    ]);
-}
 
-//check the credentials
-$user = $db->query('SELECT * FROM users WHERE email = :email', [
-    'email' => $email
-])->find();
+if ($form->validate($email, $password)) {
+    $auth = new Authenticator();
 
-//we have a user, but we don't know if the password provided matches what we have in database
-if ($user) {
-    if (password_verify($password, $user['password'])) {
-        login([
-            'email' => $email
-        ]);
-
-        header('Location: /');
-        exit();
+    if ($auth->attempt($email, $password)) {
+        redirect('/');
+    } else {
+        $form->error('email', 'The email and password does not exist.');
     }
 }
 
-return view('sessions/create.view.php', [
-    'errors' => [
-        'email' => 'The email and password does not exist.'
-    ]
+return view('sessions/create.view,php', [
+    'errors' => $form->errors()
 ]);
+
