ea66977ae3ccb2dcb7349ddd949db7841ff437ac
andreja <andrejadev@gmail.com>
2025-03-07 21:07:41 +0100

    Creating unit test - part 2
    Creating more unit test as an examples
    
    - Creating `ValidatorTest.php`
        - test: ‘`validates a string`’
        - test: ‘`validates a string with a minimum length`’
        - test: ‘`validates an email`’
        - test: ‘`validates that a number is greater than a given amount`’
    
    - Changing and creating fn in Validator.php class to satisfy the tests.

--- Core/Validator.php
+++ Core/Validator.php
@@ -10,8 +10,14 @@ class Validator
         return strlen($value) >= $min && strlen($value) <= $max;
     }
 
-    public static function email($value)
+    public static function email($value): bool
     {
         return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+
+    //writing this fn for practising PEST testing
+    public static function greaterThen(int $value, int $greaterThan): bool
+    {
+        return $value > $greaterThan;
     }
 }
--- tests/Unit/ValidatorTest.php
+++ tests/Unit/ValidatorTest.php
@@ -2,7 +2,27 @@
 
 use Core\Validator;
 
+//we can use it() instead of test() for defining a test
+
 it('validates a string', function () {
-   $result = Validator::string('foobar');
-   expect($result)->toBe(true); //we can use the method toBeTrue() as well
+    expect(Validator::string('foobar'))->toBeTrue();
+    expect(Validator::string(false))->toBeFalse();
+    expect(Validator::string(''))->toBeFalse();
+    /*Can be written like this as well:
+     * $result = Validator::string('foobar');
+    expect($result)->toBe(true); //we can use the method toBeTrue() as well
+    */
 });
+
+it('validates a string with a minimum length', function () {
+    expect(Validator::string('foobar',20))->toBeFalse();
+});
+
+it('validates an email', function () {
+    expect(Validator::email('foobar'))->toBeFalse();
+    expect(Validator::email('example@foobar.com'))->toBeTrue();
+});
+
+it('validates that a number is greater than a given amount', function () {
+    expect(Validator::greaterThen(10, 1))->toBeTrue();
+})->only(); // using PEST method only() we define that we only want to run this particular test
